af035c63f0e057a12dbec53d204645285817c381
javi-gv94 <javier.garrayo@hotmail.com>
2019-03-29 10:52:09 +0100

    updated code to adapt to new API and data model. Response is the same as before

--- table.py
+++ table.py
@@ -9,10 +9,21 @@ import pandas
 import urllib2
 import json
 import StringIO
+import requests
 
 
 # funtion that gets quartiles for x and y values
-def plot_square_quartiles(x_values, means, tools, better, percentile=50):
+def plot_square_quartiles(tools_dict, better, percentile=50):
+
+    # generate 3 lists: 
+    x_values = []
+    means = []
+    tools = []
+    for key, metrics in tools_dict.iteritems():
+        tools.append(key)
+        x_values.append(metrics[0])
+        means.append(metrics[1])
+
     x_percentile, y_percentile = (np.nanpercentile(x_values, percentile), np.nanpercentile(means, percentile))
 
     # create a dictionary with tools and their corresponding quartile
@@ -67,8 +78,16 @@ def get_quartile_points(scores_and_values, first_quartile, second_quartile, thir
 
 
 # funtion that separate the points through diagonal quartiles based on the distance to the 'best corner'
-def plot_diagonal_quartiles(x_values, means, tools, better):
-    # get distance to lowest score corner
+def plot_diagonal_quartiles( tools_dict, better):
+
+    # generate 3 lists: 
+    x_values = []
+    means = []
+    tools = []
+    for key, metrics in tools_dict.iteritems():
+        tools.append(key)
+        x_values.append(metrics[0])
+        means.append(metrics[1])
 
     # normalize data to 0-1 range
     x_norm, means_norm = normalize_data(x_values, means)
@@ -96,8 +115,19 @@ def plot_diagonal_quartiles(x_values, means, tools, better):
 
 
 # function that clusters participants using the k-means algorithm
-def cluster_tools(my_array, tools, better):
-    X = np.array(my_array)
+def cluster_tools(tools_dict, better):
+
+    # generate 3 lists: 
+    x_values = []
+    means = []
+    tools = []
+    for key, metrics in tools_dict.iteritems():
+        tools.append(key)
+        x_values.append(metrics[0])
+        means.append(metrics[1])
+
+
+    X = np.array(zip(x_values, means))
     kmeans = KMeans(n_clusters=4, n_init=50, random_state=0).fit(X)
 
     cluster_no = kmeans.labels_
@@ -148,57 +178,74 @@ def cluster_tools(my_array, tools, better):
 
 def build_table(data, classificator_id, challenge_list):
 
-    challenges = set()
-
-    for dataset in data:
-        challenge_id = dataset['_id'].split('_')[1]
-        if challenge_list == []:
-            
-            challenges.add(challenge_id)
-        else:
-            if challenge_id in challenge_list:
-                challenges.add(challenge_id)
-
+    from collections import OrderedDict
     # this dictionary will store all the information required for the quartiles table
     quartiles_table = {}
 
-    for challenge in challenges:
+    for challenge in data:
+        challenge_id = challenge['orig_id']
         x_values = []
         y_values = []
-        tools = set()
+        tools = {}
         better = 'top-right'
-        for dataset in data:
-            tools.add(dataset['depends_on']['tool_id'].split(":")[1])
-            challenge_id = dataset['_id'].split('_')[1]
-            if challenge == challenge_id:
+        # loop over all assessment datasets and create a dictionary like -> { 'tool': [x_metric, y_metric], ..., ... }
+        for dataset in challenge['datasets']:
+            if dataset['type'] == "assessment":
+                #get tool which this dataset belongs to
+                tool_id = dataset['depends_on']['tool_id']
+                if tool_id not in tools:
+                    tools[tool_id] = [0]*2
+                # get value of the two metrics
                 data_uri = dataset['datalink']['uri']
-                encoded = data_uri.split(",")[1]
+                encoded = data_uri[1]
                 metric = float(b64decode(encoded))
-                if dataset['depends_on']['metrics_id'] == "TCGA:TPR":
-                    x_values.append(metric)
-                elif dataset['depends_on']['metrics_id'] == "TCGA:PPV":
-                    y_values.append(metric)
+                if dataset['depends_on']['metrics_id'] == "OEBM0020000002":
+                    tools[tool_id][0] = metric
+                elif dataset['depends_on']['metrics_id'] == "OEBM0020000001":
+                    tools[tool_id][1] = metric
 
         # get quartiles depending on selected classification method
 
         if classificator_id == "squares":
-            tools_quartiles = plot_square_quartiles(x_values, y_values, sorted(list(tools)), better)
+            tools_quartiles = plot_square_quartiles(tools, better)
 
         elif classificator_id == "clusters":
-            tools_quartiles = cluster_tools(zip(x_values, y_values), tools, better)
+            tools_quartiles = cluster_tools(tools, better)
 
         else:
-            tools_quartiles = plot_diagonal_quartiles(x_values, y_values, sorted(list(tools)), better)
+            tools_quartiles = plot_diagonal_quartiles( tools, better)
 
-        quartiles_table[challenge] = tools_quartiles
+        quartiles_table[challenge_id] = tools_quartiles
     
     return quartiles_table
 
 def get_data(base_url, bench_id, classificator_id, challenge_list):
     try:
-        response = urllib2.urlopen(
-            base_url + 'api/scientific/Dataset/?query=' + bench_id + '+assessment&fmt=json')
-        data = json.loads(response.read())['Dataset']
+        url = base_url + "sciapi/graphql"
+        json = { 'query' : '{\
+                                getBenchmarkingEvents(benchmarkingEventFilters:{id:"'+ bench_id + '"}) {\
+                                    _id\
+                                    challenges{\
+                                    orig_id\
+                                    datasets {\
+                                        _id\
+                                        datalink{\
+                                            uri\
+                                        }\
+                                        depends_on{\
+                                            tool_id\
+                                            metrics_id\
+                                        }\
+                                        type\
+                                    }\
+                                    }\
+                                }\
+                            }' }
+
+        r = requests.post(url=url, json=json )
+        response = r.json()
+        data = response["data"]["getBenchmarkingEvents"][0]["challenges"]
+        
 
         if data == None:
             return { 'data': None}
@@ -226,7 +273,7 @@ def index_page():
 @bp.route('/<string:bench_id>/<string:classificator_id>', methods = ['POST', 'GET'])
 def compute_classification(bench_id, classificator_id="diagonals"):
 	
-    mode = "production"
+    mode = "dev"
     if mode == "production":
 	base_url = "https://openebench.bsc.es/"
     else:
